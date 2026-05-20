c3c6c0bd590d2091a721e924eed652f4a1e46ce4
Hugo <martinezgalazhugo@gmail.com>
2026-05-19 21:51:11 -0700

    AjustesIdiomas 2/3

--- js/Idiomas.js
+++ js/Idiomas.js
@@ -1,11 +1,16 @@
+const idiomaGuardado = localStorage.getItem("idioma") || "es";
+
+
+
 i18next.init({
-    lng: "es",
+    lng: idiomaGuardado,
 
     resources: {
 
         es: {
             translation: {
 
+                // Traducciones para el menú principal.
                 menu: "MENÚ",
 
                 platillos: "PLATILLOS",
@@ -43,7 +48,33 @@ i18next.init({
                 todos: "Todos",
                 categorias: "Categorías:",
 
+                //Traducciones para la descripción de los platillos.
 
+                imgdescripcionMexicano: "Platillo tradicional mexicano.",
+
+                imgdescripcionJapones: "Platillo tradicional japonés.",
+
+                imgdescripcionItaliano: "Platillo tradicional italiano.",
+
+                imgdescripcionPostre: "Postre.",
+
+                imgdescripcionBebida: "Bebida refrescante.",    
+
+                descripcion: "Descripción",
+
+                descripcion_platillo1: "Las enchiladas son un plato mexicano muy popular, consistente en tortillas de maíz rellenas de carne, queso o vegetales y cubiertas con salsa picante.",
+
+                volver_menu: "⬅ Volver al menú",
+
+                descripcion_platillo2: "Los tamales son un platillo mexicano tradicional, preparado con masa de maíz rellena de carne, queso o vegetales y envuelto en hojas de maíz para su cocción.",
+                
+                descripcion_platillo3: "El sushi es un platillo japonés tradicional preparado con arroz sazonado con vinagre, acompañado de pescado, mariscos o vegetales.",
+
+                descripcion_platillo4: "La pizza es un platillo tradicional italiano preparado con masa de pan, salsa de tomate y queso, acompañado de ingredientes variados.",
+                
+                descripcion_platillo5: "El pastel es un postre de chocolate, preparado con Chocolate.",
+
+                descripcion_platillo6: "El agua es una bebida refrescante preparada con agua mineral o embotellada."
             }
         },
 
@@ -86,6 +117,33 @@ i18next.init({
                 filtros: "Filters:",
                 todos: "All",
                 categorias: "Categories:",
+
+                //Traducciones para la descripción de los platillos.
+
+                imgdescripcionMexicano: "Traditional Mexican dish.",
+
+                imgdescripcionJapones: "Traditional Japanese dish.",
+
+                imgdescripcionItaliano: "Traditional Italian dish.",
+
+                imgdescripcionPostre: "Dessert.",
+
+                imgdescripcionBebida: "Refreshing drink.",
+
+                descripcion: "Description",
+
+                descripcion_platillo1: "Enchiladas are a very popular Mexican dish, consisting of corn tortillas filled with meat, cheese or vegetables and covered with spicy sauce.",
+
+                descripcion_platillo2: "Tamales are a traditional Mexican dish, made with corn dough filled with meat, cheese or vegetables and wrapped in corn husks for cooking.",
+
+                descripcion_platillo3: "Sushi is a traditional Japanese dish made with seasoned rice, accompanied by fish, seafood or vegetables.",
+
+                descripcion_platillo4: "Pizza is a traditional Italian dish made with bread dough, tomato sauce and cheese, topped with various ingredients.",
+                descripcion_platillo5: "Cake is a chocolate dessert, prepared with Chocolate.",
+
+                descripcion_platillo6: "Water is a refreshing drink made with mineral or bottled water.",
+
+                volver_menu: "⬅ Come back to menu"
 
 
             }
@@ -131,7 +189,34 @@ i18next.init({
                 todos: "Tutti",
                 categorias: "Categorie:",
 
+                //Traducciones para la descripción de los platillos.
 
+                imgdescripcionMexicano: "Piatto tradizionale messicano.",
+
+                imgdescripcionJapones: "Piatto tradizionale giapponese.",
+
+                imgdescripcionItaliano: "Piatto tradizionale italiano.",
+
+                imgdescripcionPostre: "Dolce.",
+
+                imgdescripcionBebida: "Bevanda rinfrescante.",
+
+                descripcion: "Descrizione",
+
+                descripcion_platillo1: "Le enchiladas sono un piatto messicano molto popolare, composto da tortillas di mais ripiene di carne, formaggio o verdure e coperte con salsa piccante.",
+
+                descripcion_platillo2: "I tamales sono un piatto tradizionale messicano, preparato con pasta di mais ripiena di carne, formaggio o verdure e avvolta in foglie di mais per la cottura.",
+
+                volver_menu: "⬅ Torna al menu",
+
+                descripcion_platillo3: "Il sushi è un piatto tradizionale giapponese preparato con riso condito, accompagnato da pesce, frutti di mare o verdure.",
+
+                descripcion_platillo4: "La pizza è un piatto tradizionale italiano preparato con pasta di pane, salsa di pomodoro e formaggio, accompagnato da ingredienti vari.",
+                descripcion_platillo5: "La torta è un dolce al cioccolato, preparato con cioccolato.",
+
+                descripcion_platillo6: "L'acqua è una bevanda rinfrescante preparata con acqua minerale o in bottiglia."
+
+            
             }
         },
 
@@ -175,32 +260,72 @@ i18next.init({
                 todos: "すべて",
                 categorias: "カテゴリー:",
 
+                //Traducciones para la descripción de los platillos.
+
+                imgdescripcionMexicano: "伝統的なメキシコ料理。",
+
+                imgdescripcionJapones: "伝統的な日本料理。",
+
+                imgdescripcionItaliano: "伝統的なイタリア料理。",
+
+                imgdescripcionPostre: "デザート。",
+
+                imgdescripcionBebida: "爽やかな飲み物。",
+
+                descripcion: "説明",
+
+                descripcion_platillo1: "エンチラーダは、トウモロコシのトルティーヤに肉、チーズ、野菜を詰め、スパイシーなソースで覆った非常に人気のあるメキシコ料理です。",
+
+                descripcion_platillo2: "タマレスは、トウモロコシの生地に肉、チーズ、野菜を詰め、トウモロコシの葉で包んで調理する伝統的なメキシコ料理です。",
+
+                descripcion_platillo3: " sushiは、酢で味付けされたお米に魚、海鮮または野菜を添えて作られる伝統的な日本料理です。",
+
+                descripcion_platillo4: "ピザは、パン生地、トマトソース、チーズをベースに、さまざまな具材をトッピングして焼き上げる伝統的なイタリア料理です。",
+
+                descripcion_platillo5: "ケーキは、チョコレートで作られるデザートです。",
+
+                descripcion_platillo6: "水は、ミネラルウォーターやボトル入りの水を使用して作られる爽やかな飲み物です。",
+
+                volver_menu: "⬅ メニューに戻る"
+
 
             }
         }
     }
+},() => {
+
+    actualizarTextos();
+
+    // Guardar el idioma seleccionado en localStorage
+    const selectorIdioma = document.getElementById("idioma");
+
+    if(selectorIdioma){
+        selectorIdioma.value = i18next.language;
+    }
 });
 
-
+//funcion para actualizar los textos de la página según el idioma seleccionado
 function actualizarTextos(){
 
     document.querySelectorAll("[data-i18n]").forEach(elemento => {
 
         const clave = elemento.getAttribute("data-i18n");
 
-        elemento.innerHTML = i18next.t(clave);
+        elemento.textContent = i18next.t(clave);
     });
 }
 
-
+//Selector del idioma 
 const selectorIdioma = document.getElementById("idioma");
 
-selectorIdioma.addEventListener("change", () => {
+if(selectorIdioma){
+    selectorIdioma.addEventListener("change", () => {
 
     const idioma = selectorIdioma.value;
+        //guardar el idioma seleccionado en localStorage
+    localStorage.setItem("idioma", idioma);
 
     i18next.changeLanguage(idioma, actualizarTextos);
 });
+}
 
-
-actualizarTextos();
